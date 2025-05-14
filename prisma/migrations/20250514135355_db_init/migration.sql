b711cddd1d2310c0b33846a514400763082ef890
André Costa <andre.costa@ipvc.pt>
2025-05-14 14:56:35 +0100

    databse migrations update

--- prisma/migrations/20250514135355_db_init/migration.sql
+++ prisma/migrations/20250514135355_db_init/migration.sql
@@ -2,6 +2,9 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "nif" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "age" INTEGER NOT NULL DEFAULT 0,
     "imageProfile" TEXT,
@@ -15,6 +18,12 @@ CREATE TABLE "User" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nif_key" ON "User"("nif");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_apiKey_key" ON "User"("apiKey");
